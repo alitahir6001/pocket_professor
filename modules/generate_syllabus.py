089e497d2400efcdc508091c42b8343286ea380f
Ali <alitahir6001@gmail.com>
2025-04-19 14:11:53 -0400

    changed name of syllabus generator file to be more action orientated

---
+++ modules/generate_syllabus.py
@@ -0,0 +1,13 @@
+import modules.llm_interaction
+
+def generate_syllabus(subject, difficulty, time, goal):    # What does this func. need to generate syllabus?
+    """
+    This function handles the syllabus generation using input from the user in app.py:
+        - get input--> subject of study + difficulty
+        - get input--> time committment (hours/week)
+        - get input--> what the user is hoping to achieve (goal)
+        - create prompt--> comprehensive system prompt to generate learning path/syllabus.
+        - call LLM-->   display the generated learning path, error out if not.
+    """
+
+pass        
