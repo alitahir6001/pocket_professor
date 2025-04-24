52cba76195a1d081843b1c9376fb280c13688bdd
Ali <alitahir6001@gmail.com>
2025-04-23 21:38:35 -0400

    clean up docstring

--- modules/generate_syllabus.py
+++ modules/generate_syllabus.py
@@ -1,13 +1,11 @@
 from modules.llm_interaction import query_llm
 import app
 
-def generate_syllabus(subject, difficulty, time, goal):    # What does this func. need to generate syllabus?
+def generate_syllabus(subject, difficulty, time, goal):
     """
-    This function handles the syllabus generation using input from the user in app.py:
-        - get input--> subject of study + difficulty
-        - get input--> time committment (hours/week)
-        - get input--> what the user is hoping to achieve (goal)
-        - create prompt--> comprehensive system prompt to generate learning path/syllabus.
-        - call LLM-->   display the generated learning path, error out if not.
+    This function handles the syllabus generation using input taken from the user in app.py:
+        1. get inputs --> (1) subject (2) difficulty (3) time committment (4) goal if they have one
+        2. create prompt --> comprehensive system prompt to generate learning path/syllabus.
+        3. LLM -->   call via Ollama to display the generated learning path, error out if not.
     """
     
